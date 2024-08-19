a2c59b2a2a855a87d6fcb39d637ad19f98c7a81c
vitttz <vitoriassilva@alu.ufc.br>
2024-08-19 10:05:44 -0300

    Teste1

--- atividade01/App.js
+++ atividade01/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import Pai from './components/01Pai';
 
 const App = () => {
@@ -6,6 +6,24 @@ const App = () => {
     <div>
       <h1>IMC</h1>
       <Pai />
+    </div>
+  );
+};
+
+export default App;*/ 
+
+
+
+import React from 'react';
+import { PlacaMae as PCPlacaMae, Memoria as PCMemoria, PlacaDeVideo as PCPlacaDeVideo } from './components/atividade01/02MeuPC';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Meu PC</h1>
+      <PCPlacaMae nome="ASUS Prime" preco={500} />
+      <PCMemoria nome="Corsair Vengeance" preco={300} />
+      <PCPlacaDeVideo nome="Nvidia RTX 3080" preco={1500} />
     </div>
   );
 };
