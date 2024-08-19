e8ab6bf2cd897462f49b30a83894bf7f92119b10
vitttz <vitoriassilva@alu.ufc.br>
2024-08-19 10:07:29 -0300

    Teste2

---
+++ Teste/01Pai.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Filho from './01Filho';
+
+const Pai = () => {
+  return (
+    <div>
+      <Filho altura={1.8} peso={90} />
+    </div>
+  );
+};
+
+export default Pai;
